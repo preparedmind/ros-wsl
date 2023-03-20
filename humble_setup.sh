500c93391ca02f27e358216e963f84bc8a1d7099
Elijah Polyakov <elijah@preparedmind.net>
2023-03-20 13:45:45 -0600

    Added humble_setup.sh which runs all commands to install ROS2 humble and colcon without input.

--- humble_setup.sh
+++ humble_setup.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 cd
 
-# Install Ros2 Humble
+# Install ROS2 Humble with commands from https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html
+
+#Set locale to UTF-8
 apt update && apt install locales
 locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
+#Ensure universe repository is enabled
 apt install software-properties-common -y
 add-apt-repository universe -y
 
+#Add ROS 2 GPG key with apt
 apt update && apt install curl -y
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
+#Add the repository to your sources list
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
+#Update and Upgrade packages
 apt update && apt upgrade -y
 
+#Install ros-humble-desktop package and set timezone
 DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt install ros-humble-desktop -y
-# create microros_ws and install microros
 
-# install espidf
+#Install colcon to build ROS2 packages
+apt install python3-colcon-common-extensions -y
 
