2059ae90e1805570026ff4b19aaf86e23088924d
Elijah Polyakov <elijah@preparedmind.net>
2023-03-20 13:41:01 -0600

    Added humble_setup.sh which runs all commands to install ROS2 humble and colcon without input.

---
+++ humble_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd
+
+# Install Ros2 Humble
+apt update && apt install locales
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+apt install software-properties-common -y
+add-apt-repository universe -y
+
+apt update && apt install curl -y
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+apt update && apt upgrade -y
+
+DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt install ros-humble-desktop -y
+# create microros_ws and install microros
+
+# install espidf
+
