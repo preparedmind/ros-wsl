7cf0ecb939808e65f4462409a785f45449c98f4b
Elijah Polyakov <elijah@preparedmind.net>
2023-03-21 23:43:25 -0600

    reordered run_all.sh to install esp-idf before microros

--- setup_scripts/run_all.sh
+++ setup_scripts/run_all.sh
@@ -2,7 +2,7 @@
 
 #Install ros2 humble
 bash humble_setup.sh
-#Install microros and create a workspace
-bash microros_ws_setup.sh
 #Install esp-idf and microros esp-idf components
 bash esp-idf_setup.sh
+#Install microros and create a workspace
+bash microros_ws_setup.sh
