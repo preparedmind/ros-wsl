7afca238f26da9d45e675031b5582178ec38b557
Elijah Polyakov <elijah@preparedmind.net>
2023-03-21 21:43:46 -0600

    esp-idf_setup.sh patched to run without user input

--- setup_scripts/esp-idf_setup.sh
+++ setup_scripts/esp-idf_setup.sh
@@ -4,7 +4,7 @@ cd
 #Setup esp-idf according to https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/linux-macos-setup.html
 
 #Get packages for Ubuntu
-apt-get install git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+apt-get install git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0 -y
 
 #This is where you would install python 3. It should already be installed if you have ros2
 
