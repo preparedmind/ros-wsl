0f4cbdc732e417efd4d4e902d2c049c260dbd1f8
root <root@crabby>
2023-04-20 18:54:08 -0400

    Removed autogenerated files

--- ros2_ws/build/key_pub/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/root/ros-wsl/ros2_ws/install/key_pub'
--- ros2_ws/src/build/key_pub/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/root/ros-wsl/ros2_ws/src/install/key_pub'
